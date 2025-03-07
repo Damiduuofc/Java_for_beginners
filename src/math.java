43bb913b8ab2cf66dfab9e5a5b123c4127fa0d14
Damidu.xd <damiduofc@gmail.com>
2025-03-07 22:45:07 +0530

    dsfsdf

--- src/math.java
+++ src/math.java
@@ -1,22 +1,24 @@
 public class math {
     public static void main (String[] args) {
-    int x = Math.max(5, 10);
-    int y = Math.min(5, 10);
-    double z = Math.sqrt(64); // square root eka ganna oni nm float use krnn
-    int a = Math.abs(-4); // absolute value eka ganna oni nm negative value eka positive krnn
-    double b = Math.random(); // random number eka ganna oni nm 0.0 to 1.0 wge
-   
-   int randomNum = (int)(Math.random() * 101); // 0.0 to 100.0 wge random number eka ganna oni nm
-   
-   
-   
+        int x = Math.max(5, 10);
+        int y = Math.min(5, 10);
+        double z = Math.sqrt(64); // square root eka ganna oni nm float use krnn
+        int a = Math.abs(-4); // absolute value eka ganna oni nm negative value eka positive krnn
+        double b = Math.random(); // random number eka ganna oni nm 0.0 to 1.0 wge
+        int randomNum = (int)(Math.random() * 101); // 0.0 to 100.0 wge random number eka ganna oni nm
+        double num = 10.99;
+        int intNum = (int) Math.round(num);
    
    
-    System.out.println(x);
-    System.out.println(y);
-    System.out.println(z);
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(randomNum);
-} 
+   
+   
+   
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(randomNum);
+        System.out.println(intNum);
+    } 
 }
